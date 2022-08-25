1257672f779e985805725277f6b6c5a9e2f6cc40
Danny Textores <textores.danny@gmail.com>
2022-08-25 19:46:05 +0200

    add exception formatter

---
+++ src/ExceptionFormatter.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./ExceptionFormatter.css"
+
+export function ExceptionFormatter() {
+  const [text, setText] = useState("")
+
+  let inactive = ""
+  if (text !== "") {
+    inactive = "formatter-hint--inactive"
+  }
+
+  return <div className="wrapper">
+    <div className={`formatter-hint ${inactive}`} >
+      <h2>Paste escaped exception here...</h2>
+    </div>
+    <textarea
+      className="code"
+      onChange={ event => setText(formatException(event.target.value)) }
+      value={ text }
+    ></textarea>
+  </div>;
+}
+
+function formatException(text: string): string {
+  let result = text
+
+  if (result.startsWith("\"")) {
+    result = result.substring(1)
+  }
+  if (result.endsWith("\"")) {
+    result = result.substring(0, result.length - 1)
+  }
+
+  result = result.replaceAll("\\n", "\n")
+  result = result.replaceAll("\\t", "\t")
+
+  return result
+}
